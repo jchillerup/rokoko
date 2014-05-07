50dc3f12ab7be3589639f92a441859b38a752062
Jens Christian Hillerup <jens@hillerup.net>
2014-05-07 17:25:17 +0200

    now sends osc packages

--- imu-udp-bridge/main.py
+++ imu-udp-bridge/main.py
@@ -1,12 +1,13 @@
 import sys, signal
 import serial
+from pythonosc import osc_message_builder, udp_client
 
 NODES = ["/dev/ttyACM6", "/dev/ttyACM7", "/dev/ttyACM8",  "/dev/ttyACM9", "/dev/ttyACM10",]
 
 ports = []
 
 def signal_handler(signal, frame):
-    print "Exiting gracefully"
+    print("Exiting gracefully")
         
     for port in ports:
         port.close();
@@ -16,15 +17,17 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 
-class Port:
+class Port():
     devnode = None
+    shortname = None
     descriptor = None
     in_sync = False
     
     def __init__(self, devnode):
-        print "Opening %s" % devnode
+        print( "Opening %s" % devnode)
 
         self.devnode = devnode
+        self.shortname = devnode.split("/")[2]
         self.descriptor = serial.Serial(device, 57600)
 
         self.sync()
@@ -32,33 +35,46 @@ class Port:
     def sync(self):
         cur = 'x'
 
-        while cur != '&':
+        while cur != b'&':
             cur = self.descriptor.read()
-
-        print "Synced"
+            
+        print ("Synced")
         self.in_sync = True
 
-    def get_next_reading(self):
-        reading = ""
+    def get_reading(self):
+        reading = b""
         cur = 'x'
-        while cur != '&':
+        while cur != b'&':
             cur =  self.descriptor.read()
             reading += cur
         
-        return reading
+        return str(reading)
 
+    def get_osc(self):
+        msg = osc_message_builder.OscMessageBuilder(address = ("/sensors/%s" % self.shortname))
+        msg.add_arg(self.get_reading(), osc_message_builder.OscMessageBuilder.ARG_TYPE_STRING)
+
+        return msg.build()
+    
     def close(self):
         return self.descriptor.close()
         
 
 if __name__ == '__main__':
+    client = udp_client.UDPClient("127.0.0.1", 35000)
+
     for device in NODES:
         ports.append(Port(device))
 
+    num_packets = 0;
+        
     while True:
         for port in ports:
-            reading = port.get_next_reading().replace("\n", " *** ")
-            print "%s: %s" % (port.devnode, reading)
-            
+            package = port.get_osc()
+            client.send(package)
+            num_packets += 1
+
+            if num_packets % 100 == 0:
+                print ("Packet count: %d" % num_packets)
     
 

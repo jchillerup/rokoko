56172921d7d8290be5c83615290f74c2242c3656
Jens Christian Hillerup <jens@hillerup.net>
2014-05-15 18:11:33 +0200

    bugfixes and UI controls

--- imuTestVisualizer/addons.make
+++ imuTestVisualizer/addons.make
@@ -1,3 +1,4 @@
 ofxSkeleton
 ofxXmlSettings
 ofxOsc
+ofxUI
--- imuTestVisualizer/src/ofApp.h
+++ imuTestVisualizer/src/ofApp.h
@@ -4,72 +4,73 @@
 #include "ofxSkeleton.h"
 #include "Imu.h"
 #include "ofxOsc.h"
+#include "ofxUI.h"
 
-#define NUM_SENSORS 5
+#define NUM_SENSORS 3
 
 class ofApp : public ofBaseApp{
 	
-	typedef shared_ptr<pal::ofxSkeleton::ofxJoint>  JointP_t;
+  typedef shared_ptr<pal::ofxSkeleton::ofxJoint>  JointP_t;
 	
-	//vector<JointP_t>							mSkeleton;
+  //vector<JointP_t>							mSkeleton;
 	
-	ofEasyCam										mCam1;
-	map<string, JointP_t>							mSkeleton;
+  ofEasyCam										mCam1;
+  map<string, JointP_t>							mSkeleton;
     
 
-public:
-	void setup();
-	void update();
-	void draw();
+ public:
+  void setup();
+  void update();
+  void draw();
 	
-    void exit();
-	void keyPressed(int key);
-	void keyReleased(int key);
-	void mouseMoved(int x, int y );
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
-	void windowResized(int w, int h);
-	void dragEvent(ofDragInfo dragInfo);
-	void gotMessage(ofMessage msg);
+  void exit();
+  void keyPressed(int key);
+  void keyReleased(int key);
+  void mouseMoved(int x, int y );
+  void mouseDragged(int x, int y, int button);
+  void mousePressed(int x, int y, int button);
+  void mouseReleased(int x, int y, int button);
+  void windowResized(int w, int h);
+  void dragEvent(ofDragInfo dragInfo);
+  void gotMessage(ofMessage msg);
     
-    ofSerial serial;
+  ofSerial serial;
     
-    // data from IMU
-    ofQuaternion rawQuarternion;
-    ofVec3f rawGyro;
-    ofVec3f rawAccel;
-    ofVec3f rawMag;
+  // data from IMU
+  ofQuaternion rawQuarternion;
+  ofVec3f rawGyro;
+  ofVec3f rawAccel;
+  ofVec3f rawMag;
     
-    ofQuaternion dmpQuaternion;
-    ofVec3f dmpEulerPose;
-    ofVec3f calAccel;
-    ofVec3f calMag;
+  ofQuaternion dmpQuaternion;
+  ofVec3f dmpEulerPose;
+  ofVec3f calAccel;
+  ofVec3f calMag;
     
-    ofQuaternion fusedQuaternion;
-    ofVec3f fusedEulerPose;
+  ofQuaternion fusedQuaternion;
+  ofVec3f fusedEulerPose;
     
-    ofVec3f residualAccel;
-    ofVec3f velocity;
-    ofVec3f position;
+  ofVec3f residualAccel;
+  ofVec3f velocity;
+  ofVec3f position;
     
-    ofVec3f calZeroVector;
+  ofVec3f calZeroVector;
     
-    ofEasyCam cam;
+  ofEasyCam cam;
     
-    int long calTime;
-    int long firstData;
-    bool dataReceived;
+  int long calTime;
+  int long firstData;
+  bool dataReceived;
     
-    ofVec3f offset;
+  ofVec3f offset;
     
-    int state;
+  int state;
     
-    vector<Imu *> imus;
+  vector<Imu *> imus;
     
-    bool debugdraw;
+  bool debugdraw;
     
-    ofxOscSender oscSender;
-    
+  ofxOscSender oscSender;
+  ofxUICanvas * gui;
 };
 

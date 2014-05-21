dd272ca22f6fe22f95ea1a3fc0d263615c5f238e
Jens Christian Hillerup <jens@hillerup.net>
2014-05-21 10:35:47 +0200

    measuring time

--- imu-udp-bridge/main.py
+++ imu-udp-bridge/main.py
@@ -127,6 +127,7 @@ if __name__ == '__main__':
     #         time.sleep(1/len(NODES))
 
 
+    start = time.time();
     while True:
         num_packets = 0
         
@@ -136,6 +137,6 @@ if __name__ == '__main__':
             num_packets += 1
 
             if num_packets % 100 == 0:
-                print ("Packet count: %d" % num_packets)
+                print ("Packet count: %d, %f seconds" % (num_packets, time.time()-start)
     
 

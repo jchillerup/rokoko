cc6043cf857f797842e61f7681484e75bad4bf8c
Jens Christian Hillerup <jens@hillerup.net>
2014-05-23 22:18:25 +0200

    boom! threaded IMU bridge in C

--- imu-udp-bridge-c/main.c
+++ imu-udp-bridge-c/main.c
@@ -21,50 +21,6 @@ int open_device(char* address, struct termios * tio) {
   return tty_fd;
 }
 
-void work_sensor(int tty_fd, lo_address* recipient) {
-  const unsigned char GET_READING_BYTE = 'g';
-  const unsigned char GET_IDENTIFIER_BYTE = 'i';
-  
-  int num_packets = 0;
-  int fail_packets = 0;
-  char sensor_ident[16];
-
-  // Prepare a datastructure for receiving payloads
-  float * payload = malloc(4 * sizeof(float));
-
-  // Get the ident of the sensor
-  write(tty_fd, &GET_IDENTIFIER_BYTE, 1);
-  assert(read(tty_fd, sensor_ident, 16) == 16);
-  printf("Sensor ident: %s\n", sensor_ident);
-
-  // Loop if DEBUG is false OR if it's true and num_packets < 100
-  while ( (DEBUG ^ 1) || (DEBUG && num_packets < 100) )
-    {
-      int read_out = 0;
-      // Write a 'g' to the Arduino
-      write(tty_fd, &GET_READING_BYTE, 1);
-
-      // Put the output from the Arduino in `payload'. `read' will block.
-      read_out = read(tty_fd, payload, 16);
-      num_packets++;
-
-      if (read_out < 16) {
-        printf("short read\n");
-        fail_packets++;
-        continue;
-      }
-      
-      if (DEBUG)
-        printf("%.2f, %.2f, %.2f, %.2f\n", payload[1], payload[2], payload[3], payload[0]);
-
-      // Put the payload into an OSC message...
-      lo_send(recipient, "/sensor", "sffff", sensor_ident, payload[1], payload[2], payload[3], payload[0]);
-    }
-
-  printf("num_packets: %d, fail_packets: %d\n", num_packets, fail_packets);
-  
-  free(payload);
-}
 
 void prepare_terminal(struct termios * tio) {
   memset(tio, 0, sizeof(*tio));
@@ -76,11 +32,72 @@ void prepare_terminal(struct termios * tio) {
   tio->c_cc[VTIME] = 5;
 }
 
+
+typedef struct sensor_args {
+  char * devnode;
+  int tty_fd;
+  lo_address* recipient;
+} sensor_args;
+
+
+void * work_sensor(void * v_args) {
+  sensor_args * args = (sensor_args *) v_args;
+  
+  const unsigned char GET_READING_BYTE = 'g';
+  const unsigned char GET_IDENTIFIER_BYTE = 'i';
+  
+  int num_packets = 0;
+  int fail_packets = 0;
+  char sensor_ident[16];
+
+  // Prepare a datastructure for receiving payloads.
+  float * payload = malloc(4 * sizeof(float));
+
+  printf("devnode: %s\n", args->devnode);
+  printf("tty:     %d\n", args->tty_fd);
+  
+  // Get the ident of the sensor
+  write(args->tty_fd, &GET_IDENTIFIER_BYTE, 1);
+  read(args->tty_fd, sensor_ident, 16);
+  printf("Sensor ident: %s\n", sensor_ident);
+  
+  // Loop if DEBUG is false OR if it's true and num_packets < 100.
+  while ( (DEBUG ^ 1) || (DEBUG && num_packets < 100) )
+    {
+      int read_out = 0;
+      // Write a 'g' to the Arduino
+      write(args->tty_fd, &GET_READING_BYTE, 1);
+
+      // Put the output from the Arduino in `payload'. `read' will block.
+      read_out = read(args->tty_fd, payload, 16);
+      num_packets++;
+
+      if (read_out < 16) {
+        fail_packets++;
+        continue;
+      }
+      
+      if (DEBUG)
+        printf("%.2f, %.2f, %.2f, %.2f\n", payload[1], payload[2], payload[3], payload[0]);
+
+      // Put the payload into an OSC message.
+      lo_send(*(args->recipient), "/sensor", "sffff", sensor_ident, payload[1], payload[2], payload[3], payload[0]);
+    }
+
+  printf("num_packets: %d, fail_packets: %d\n", num_packets, fail_packets);
+  
+  free(payload);
+
+  pthread_exit(NULL);
+}
+
+
 int main(int argc,char** argv)
 {
   // We allocate as many threads as we have arguments for the program, one for each device
   // to handle.
   pthread_t threads[ argc - 1 ];
+  sensor_args arg_structs[ argc - 1 ];
   int i;
   struct termios tio;
   
@@ -90,14 +107,31 @@ int main(int argc,char** argv)
   }
   
   prepare_terminal(&tio);
-  
+
+  // Loop through all commandline args and start a thread for each sensor we
+  // want to look at.
   for (i = 1; i < argc; i++) {
     printf("Opening %s\n", argv[i]);
-    
-    int fd               = open_device(argv[i], &tio);
+
+    int fd = open_device(argv[i], &tio);
     lo_address recipient = lo_address_new(RECIPIENT, "14040");
-    work_sensor(fd, &recipient);
-    close(fd);
+    
+    // Construct the arguments struct and spawn a thread
+    //sensor_args * args = malloc(sizeof(sensor_args));
+    arg_structs[i-1].devnode = argv[i];
+    arg_structs[i-1].tty_fd = fd;
+    arg_structs[i-1].recipient = &recipient;
+
+    pthread_create(&threads[i-1], NULL, work_sensor, &arg_structs[i-1]);
+  }
+
+  for (i = 0; i < argc-1; i++) {
+    pthread_join(threads[i], NULL);
+  }
+  
+  // Clean up the file descriptors
+  for (i = 0; i< argc-1; i++)  {
+    close(arg_structs[i].tty_fd);
   }
   
   return EXIT_SUCCESS;

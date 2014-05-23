7974ef508e1eb22744c27d3fe51aaece7aa64ebf
Jens Christian Hillerup <jens@hillerup.net>
2014-05-23 13:06:31 +0200

    major refactoring of the c code

--- imu-udp-bridge-c/main.c
+++ imu-udp-bridge-c/main.c
@@ -8,47 +8,34 @@
 #include <lo/lo.h>
 #include "settings.h"
 
-int main(int argc,char** argv)
-{
-  struct termios tio;
+// Sets up a device for dumping data. Returns a file descriptor
+int open_device(char* address, struct termios * tio) {
   int tty_fd;
-  
+
+  tty_fd = open(address, O_RDWR | O_NOCTTY);
+  cfsetospeed(tio, B115200);            // 115200 baud
+  cfsetispeed(tio, B115200);            // 115200 baud
+  tcsetattr(tty_fd, TCSANOW, tio);
+
+  return tty_fd;
+}
+
+void work_sensor(int tty_fd, lo_address* recipient) {
   const unsigned char GET_READING_BYTE = 'g';
   const unsigned char GET_IDENTIFIER_BYTE = 'i';
   
   int num_packets = 0;
   int fail_packets = 0;
-  
-  if (argc == 1) {
-    printf("No device node given\n");
-    return(255);
-  }
-  
-  char* sensor_devnode = argv[1];
   char sensor_ident[16];
-  
-  memset(&tio,0,sizeof(tio));
-  tio.c_iflag     = 0;
-  tio.c_oflag     = 0;
-  tio.c_cflag     = CS8|CREAD|CLOCAL;           // 8n1, see termios.h for more information
-  tio.c_lflag     = 0;
-  tio.c_cc[VMIN]  = 16;
-  tio.c_cc[VTIME] = 5;
 
-  tty_fd = open(sensor_devnode, O_RDWR | O_NOCTTY);
-  cfsetospeed(&tio, B115200);            // 115200 baud
-  cfsetispeed(&tio, B115200);            // 115200 baud
-  tcsetattr(tty_fd, TCSANOW, &tio);
-  
+  // Prepare a datastructure for receiving payloads
   float * payload = malloc(4 * sizeof(float));
-  lo_address recipient = lo_address_new(RECIPIENT, "14040");
 
+  // Get the ident of the sensor
   write(tty_fd, &GET_IDENTIFIER_BYTE, 1);
-
   assert(read(tty_fd, sensor_ident, 16) == 16);
-
   printf("Sensor ident: %s\n", sensor_ident);
-  
+
   // Loop if DEBUG is false OR if it's true and num_packets < 100
   while ( (DEBUG ^ 1) || (DEBUG && num_packets < 100) )
     {
@@ -66,17 +53,6 @@ int main(int argc,char** argv)
         continue;
       }
       
-      // BUG: Sometimes the readings come out wrong. We filter these out (but really, the bug should be fixed)
-      /* if ( */
-      /*     payload[0] > 1.0 || payload[0] < -1.0 || */
-      /*     payload[1] > 1.0 || payload[1] < -1.0 || */
-      /*     payload[2] > 1.0 || payload[2] < -1.0 || */
-      /*     payload[3] > 1.0 || payload[3] < -1.0 */
-      /*     ) { */
-      /*   fail_packets++; */
-      /*   continue; */
-      /* } */
-
       if (DEBUG)
         printf("%.2f, %.2f, %.2f, %.2f\n", payload[1], payload[2], payload[3], payload[0]);
 
@@ -85,9 +61,32 @@ int main(int argc,char** argv)
     }
 
   printf("num_packets: %d, fail_packets: %d\n", num_packets, fail_packets);
-
+  
   free(payload);
-  close(tty_fd);
+}
 
+int main(int argc,char** argv)
+{
+  
+  if (argc == 1) {
+    printf("No device node(s) given\n");
+    return(255);
+  }
+  
+  
+  struct termios tio;
+  memset(&tio,0,sizeof(tio));
+  tio.c_iflag     = 0;
+  tio.c_oflag     = 0;
+  tio.c_cflag     = CS8|CREAD|CLOCAL;           // 8n1, see termios.h for more information
+  tio.c_lflag     = 0;
+  tio.c_cc[VMIN]  = 16;
+  tio.c_cc[VTIME] = 5;
+
+  int fd               = open_device(argv[1], &tio);
+  lo_address recipient = lo_address_new(RECIPIENT, "14040");
+  work_sensor(fd, &recipient);
+  close(fd);
+  
   return EXIT_SUCCESS;
 }

e35997722b86c053640a4b7f0b9c49ea8ac0684a
Jens Christian Hillerup <jens@hillerup.net>
2014-06-17 16:00:21 +0200

    added a calibration routine

--- imu-debugger/imu.py
+++ imu-debugger/imu.py
@@ -1,4 +1,4 @@
-import os, sys, serial, struct
+import os, sys, serial, struct, time
 
 class IMU:
     descriptor = None
@@ -22,6 +22,45 @@ class IMU:
         self.identifier = self.descriptor.read(16).decode('ascii').rstrip()
 
         return self.identifier
+
+    def calibrate_accelerometer(self):
+        self.descriptor.write(b'a')
+        time.sleep(0.25)
+
+        print(self.descriptor.readline())
+        time.sleep(10);
+        print(self.descriptor.readline())
+        print(self.descriptor.readline())
+
+        return True
+        # if self.descriptor.read(3) == 'ca\n':
+        #     time.sleep(10)
+
+        #     if self.descriptor.read(5) == 'AS\nCA':
+        #         return True
+        # else:
+        #     return False
+
+    def calibrate_magnometer(self):
+        self.descriptor.write(b'm')
+        time.sleep(0.25)
+
+        print(self.descriptor.readline())
+        time.sleep(10);
+        print(self.descriptor.readline())
+        print(self.descriptor.readline())
+
+        return True
+                
+        # if self.descriptor.read(3) == 'cm\n':
+        #     time.sleep(10)
+
+        #     if self.descriptor.read(5) == 'MS\nCM':
+        #         return True
+        # else:
+        #     return False
+
+    
 
     def rename(self, new_name):
         if (len(new_name) > 16):

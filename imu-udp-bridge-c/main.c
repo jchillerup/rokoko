4e93db49f12c26e6b3393cb5d4c85480703a81f2
Jens Christian Hillerup <jens@hillerup.net>
2014-06-23 15:12:50 +0200

    threading

--- imu-udp-bridge-c/main.c
+++ imu-udp-bridge-c/main.c
@@ -8,16 +8,11 @@
 #include <lo/lo.h>
 #include "settings.h"
 #include <time.h>
+#include <pthread.h>
 
 const unsigned char GET_READING_BYTE = 'g';
 const unsigned char GET_IDENTIFIER_BYTE = 'i';
 
-typedef struct sensor_args {
-  char * devnode;
-  int tty_fd;
-  lo_address* recipient;
-  char * ident;
-} sensor_args;
 
 typedef struct sensor_reading {
   char * ident;
@@ -27,8 +22,17 @@ typedef struct sensor_reading {
   float z;
 } sensor_reading;
 
-struct timespec rw_delay;
+typedef struct sensor_args {
+  char * devnode;
+  int tty_fd;
+  lo_address* recipient;
+  char * ident;
+  sensor_reading reading;
+} sensor_args;
 
+
+struct timespec rw_delay;
+struct timespec net_delay;
 
 // Sets up a device for dumping data. Returns a file descriptor
 int open_device(char* address, struct termios * tio) {
@@ -66,29 +70,31 @@ void * prepare_sensor(sensor_args * args) {
   printf("Sensor ident: %s\n", args->ident);
 }
 
-void get_reading(sensor_args * args, sensor_reading* reading) {
+void * get_reading(void * v_args) {
   int read_out = 0;
-  
+  sensor_args * args = (sensor_args*) v_args;
+    
   // Prepare a datastructure for receiving payloads.
   float * payload = malloc(4 * sizeof(float));
 
-  // Write a 'g' to the Arduino
-  write(args->tty_fd, &GET_READING_BYTE, 1);
+  while(1) {
+    // Write a 'g' to the Arduino
+    write(args->tty_fd, &GET_READING_BYTE, 1);
 
-  // Sleep for a while
-  nanosleep(&rw_delay, NULL);
+    // Sleep for a while
+    nanosleep(&rw_delay, NULL);
   
-  // Put the output from the Arduino in `payload'. `read' will block.
-  read_out = read(args->tty_fd, payload, 16);
-
-  if (read_out == 16) {
-    reading->w = payload[0];
-    reading->x = payload[1];
-    reading->y = payload[2];
-    reading->z = payload[3];
+    // Put the output from the Arduino in `payload'. `read' will block.
+    read_out = read(args->tty_fd, payload, 16);
     
-    // memcpy(w, payload, 4* sizeof(float)); // We're overwriting x, y, z as well.
-    reading->ident = args->ident;
+    if (read_out == 16) {
+      args->reading.w = payload[0];
+      args->reading.x = payload[1];
+      args->reading.y = payload[2];
+      args->reading.z = payload[3];
+
+      //reading->ident = args->ident;
+    }
   }
   free(payload);
 }
@@ -106,10 +112,15 @@ int main(int argc,char** argv)
   char * r_ptr = fp_recipient;
   int num_sensors = argc - 1;
   sensor_reading * sensors = calloc(num_sensors, sizeof(sensor_reading));
-
+  pthread_t threads[argc-1];
+  
   // Set the delay to wait before polling for an answer
   rw_delay.tv_sec = 0;
-  rw_delay.tv_nsec = 7 * 1e6; // 1000000 ns = 1ms
+  rw_delay.tv_nsec = 8 * 1e6; // 1000000 ns = 1ms
+
+  // Set the delay between network packets being sent
+  net_delay.tv_sec = 0;
+  net_delay.tv_nsec = 50*1e6;
   
   // Read the IP of the recipient from recipient.txt
   file = fopen( "recipient.txt" , "r");
@@ -146,26 +157,32 @@ int main(int argc,char** argv)
     arg_structs[i-1].tty_fd = fd;
     arg_structs[i-1].recipient = &recipient;
     arg_structs[i-1].ident = calloc(16, sizeof(char));
+    arg_structs[i-1].reading = sensors[i-1];
     
     prepare_sensor(&arg_structs[i-1]);
   }
 
-  while (num_packets < 100) {
-    // Keep reading them
-    for (i=0; i<argc-1; i++) {
-      get_reading(&arg_structs[i], &(sensors[i]));
-    }
-
-    // Send sensors to the recipient
-    for (i = 0; i< num_sensors; i++) {
-      printf("%s %.02f %.02f %.02f %.02f\n", sensors[i].ident, sensors[i].w, sensors[i].x, sensors[i].y, sensors[i].z);
-    }
-
-    printf("*** %d ***\n", num_packets);
-
-    num_packets++;
+  // Keep reading them
+  for (i = 0; i < argc-1; i++) {
+    pthread_create(&threads[i], NULL, get_reading, &arg_structs[i]);
   }
 
+  // Send sensors to the recipient
+  /* for (i = 0; i< num_sensors; i++) { */
+  /*   printf("%s %.02f %.02f %.02f %.02f\n", sensors[i].ident, sensors[i].w, sensors[i].x, sensors[i].y, sensors[i].z); */
+  /* } */
+
+  while (1) {
+    for(i=0; i< argc-1; i++) {
+      printf("%.02f %.02f %.02f %.02f\n", arg_structs[i].reading.w, arg_structs[i].reading.x,  arg_structs[i].reading.y, arg_structs[i].reading.z);
+    }
+    nanosleep(&net_delay, NULL);
+  }
+
+  for (i=0; i< argc-1; i++) {
+    pthread_join(threads[i], NULL);
+  }
+  
   
   // Clean up the file descriptors
   for (i = 0; i< argc-1; i++)  {

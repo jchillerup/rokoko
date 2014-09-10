37f5da4c3c55be5e438c95309a01b966b0c13068
root <root@rokoko-udoo1.lan>
2014-09-10 15:46:03 +0200

    improved sensor data sanity check

--- imu-udp-bridge-c/prepare_sensor.c
+++ imu-udp-bridge-c/prepare_sensor.c
@@ -1,10 +1,10 @@
 int prepare_sensor (void * v_args) {
-  int i;
   sensor_args * args = (sensor_args *) v_args;
   
   const unsigned char GET_IDENTIFIER_BYTE = 'i';
   const unsigned char GET_TYPE_BYTE = 't';
-    
+  int i = 0;
+
   char * sensor_ident = args->sensor_ident;
   char sensor_type_string[16];
   
@@ -15,37 +15,47 @@ int prepare_sensor (void * v_args) {
   // Flush any data that might be in the serial buffer already
   tcflush(args->tty_fd, TCIOFLUSH);
 
-  sleep(1);
+  sleep(2);
 
   //while( ~((sensor_ident[0] >= '0' && sensor_ident[0] <= '9') || (sensor_ident[0] >= 'A' && sensor_ident[0] <= 'Z')) )  {
-  while(sensor_ident[0] < 0x30 || sensor_ident[0] > 0x5a) {
+  while(i == 0 || sensor_ident[0] < 0x30 || sensor_ident[0] > 0x5a) {
     // Get the ident of the sensor
     write(args->tty_fd, &GET_IDENTIFIER_BYTE, 1);
 #ifdef SLEEP
     nanosleep(&delay_before_read, NULL);
 #endif
-    read(args->tty_fd, args->sensor_ident, 16);
+    read(args->tty_fd, sensor_ident, 16);
+
+    i++;
   }
   
 #ifdef SLEEP
   nanosleep(&delay_before_read, NULL);
 #endif
   
-  while(sensor_type_string[0] < 0x30 || sensor_type_string[0] > 0x5a) {
+  i=0;
+  while(i == 0 || sensor_type_string[0] < 0x30 || sensor_type_string[0] > 0x5a) {
     // Get the sensor type
     write(args->tty_fd, &GET_TYPE_BYTE, 1);
 #ifdef SLEEP
     nanosleep(&delay_before_read, NULL);
 #endif
     read(args->tty_fd, sensor_type_string, 16);
+
+    i++;
   }
   
   // clear out some of the bytes of the name
+
+  for (i=6; i<16; i++) {
+    sensor_type_string[i] = 0;
+  }
+
   for (i=2; i<16; i++) {
     sensor_ident[i] = 0;
   }
  
-  printf("%s: %s\n", args->devnode, sensor_ident);
+  printf("%s: %s (%s)\n", args->devnode, sensor_ident, sensor_type_string);
 
   if (sensor_type_string[0] == 'I') {
     args->sensor_type = ROKOKO_IMU;

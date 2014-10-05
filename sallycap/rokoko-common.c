f6c72faf069a84175ad8cdc936002f3f11df9227
Jens Christian Hillerup <jens@hillerup.net>
2014-10-05 15:59:23 +0200

    forgot this

--- sallycap/rokoko-common.c
+++ sallycap/rokoko-common.c
@@ -1,22 +1,27 @@
 /** Constants **/
 #define MAX_CONTOURS 21 /*;*/
 
+typedef struct rokoko_contour {
+  cv::Point center;
+  //char label[20];
+};
+
 /** typedefs **/
 typedef struct rokoko_face {
   int num_contours;
-  cv::Point contours[MAX_CONTOURS];
+  rokoko_contour contours[MAX_CONTOURS];
   cv::Point left_eye;
   cv::Point right_eye;
 } rokoko_face;
 
-/* void pretty_print_face(rokoko_face* cur_face) { */
-/*   printf("Left pupil: (%d, %d)\n", cur_face->left_eye.x, cur_face->left_eye.y); */
-/*   printf("Right pupil: (%d, %d)\n", cur_face->right_eye.x, cur_face->right_eye.y); */
+void pretty_print_face(rokoko_face* cur_face) {
+  printf("Left pupil: (%d, %d)\n", cur_face->left_eye.x, cur_face->left_eye.y);
+  printf("Right pupil: (%d, %d)\n", cur_face->right_eye.x, cur_face->right_eye.y);
   
-/*   printf("Number of contours: %d\n", cur_face->num_contours); */
+  printf("Number of contours: %d\n", cur_face->num_contours);
   
-/*   for (int i = 0; i < cur_face->num_contours; i++) { */
-/*     printf(" - Contour #%d: %d, %d\n", i, cur_face->contours[i].x, cur_face->contours[i].y); */
-/*   } */
-/*   printf("\n"); */
-/* } */
+  for (int i = 0; i < cur_face->num_contours; i++) {
+    printf(" - C#%d\t%d\t%d\t\n", i, cur_face->contours[i].center.x, cur_face->contours[i].center.y /*, cur_face->contours[i].label*/);
+  }
+  printf("\n");
+}

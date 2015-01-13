02d87eeb1e0853fcdf20eeff59dfae36b3190c8c
Jens Christian Hillerup <jens@hillerup.net>
2015-01-13 12:07:38 +0100

    adding a python client for the pixy arduino sketch

--- pixyclient/client.py
+++ pixyclient/client.py
@@ -3,34 +3,36 @@ import sys, os, time, struct, serial
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
-ANDERS = "192.168.0.109"
+ANDERS = "192.168.0.114"
 osc_client = udp_client.UDPClient(ANDERS, 14040)
 
-ser = serial.Serial(sys.argv[1], 115200)
+ser = serial.Serial(sys.argv[1], 9600)
 
 def get_packet():
     pl = ""
     num_blocks = -1;
     buf = ""
 
+    ser.write('h')
+    
     while "*" not in pl:
         pl = ser.readline().decode("utf-8").rstrip()
 
     num_blocks = int(pl[1:])
-
+    
     buf = struct.pack("i", num_blocks)
 
     for i in range(num_blocks):
         block = ser.readline().decode("utf-8").rstrip().split(";")
-        buf += struct.pack("ii", int(block[1]), int(block[2]))
+        buf += struct.pack("iiiii", int(block[1]), int(block[2]), int(block[3]), int(block[4]), int(block[5]))
 
     # add contours to make total = 21
     for i in range(21-num_blocks):
-        buf += struct.pack("ii", 0, 0)
+        buf += struct.pack("iiiii", -1, 0, 0, -1, -1)
 
     # add eyes
-    buf += struct.pack("ii", 0, 0)
-    buf += struct.pack("ii", 0, 0)
+    buf += struct.pack("iiiii", -1, 0, 0, -1, -1)
+    buf += struct.pack("iiiii", -1, 0, 0, -1, -1)
 
     return buf
 
